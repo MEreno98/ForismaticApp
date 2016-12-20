047561f88d69a50e003dd7acb35c37dcf3f73916
Mikel Ereño <MEreno1995@hotmail.com>
2016-12-20 20:40:14 +0100

    Initial Commit

---
+++ ForismaticApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForismaticApp
+//
+//  Created by Mikel Ereño on 20/12/16.
+//  Copyright © 2016 Mikel Ereño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
