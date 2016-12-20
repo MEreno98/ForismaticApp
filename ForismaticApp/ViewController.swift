7857c67bf88fab9ef1d6c6e2b4793eee0cf00913
Mikel Ereño <MEreno1995@hotmail.com>
2016-12-20 21:51:31 +0100

    Cambio de Idioma

--- ForismaticApp/ViewController.swift
+++ ForismaticApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var l_autor: UILabel!
+    @IBOutlet weak var l_frase: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sc_idioma(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 1:
+            //Ruso
+            l_autor.text = "Автор:"
+            l_frase.text = "Выражение:"
+            sender.setTitle("Aнглийский", forSegmentAt: 0)
+            sender.setTitle("Pусский", forSegmentAt: 1)
+        default:
+            //Ingles
+            l_autor.text = "Author:"
+            l_frase.text = "Expression:"
+            sender.setTitle("English", forSegmentAt: 0)
+            sender.setTitle("Russian", forSegmentAt: 1)
+        }
+    }
 
 }
 
